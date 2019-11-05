a985b4eb90bc4d1ace9ac9ce579f47bff6464592
Donovan Mandap <donovan@coremechanic.com>
2019-11-04 20:54:21 -0800

    Delete the "pause" in our build system .bat files

--- armgcc/build_all.bat
+++ armgcc/build_all.bat
@@ -2,4 +2,3 @@ cmake -DCMAKE_TOOLCHAIN_FILE="./armgcc.cmake" -G "MinGW Makefiles" -DCMAKE_BUILD
 mingw32-make -j4
 cmake -DCMAKE_TOOLCHAIN_FILE="./armgcc.cmake" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release  .
 mingw32-make -j4
-IF "%1" == "" ( pause )
--- armgcc/clean.bat
+++ armgcc/clean.bat
@@ -1,3 +1,2 @@
 RD /s /Q debug release CMakeFiles
-DEL /s /Q /F Makefile cmake_install.cmake CMakeCache.txt
-pause
+DEL /s /Q /F Makefile cmake_install.cmake CMakeCache.txt build_log.txt
