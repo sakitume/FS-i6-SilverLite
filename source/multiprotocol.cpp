2447554828ea7bff04507b586f31fdf4af7cb897
Donovan Mandap <donovan@coremechanic.com>
2019-12-17 02:48:52 -0800

    Switches on FlySky now mapped to various Bayang flags

--- source/multiprotocol.cpp
+++ source/multiprotocol.cpp
@@ -233,12 +233,106 @@ void multiprotocol_update(void)
             uint8_t data;
 
             // TODO: Put these into a model definition structure
-            const uint8_t settings_protocol = 14;       // 14 == Bayang
+            const uint8_t kBayangProtocol = 14;
+            const uint8_t settings_protocol = kBayangProtocol;
             const uint8_t settings_sub_protocol = 0;    // 0 == Bayang
+            const uint8_t settings_option = 1;          // 1 == Bayang telemetry. Note: When using test RX board, don't use telemetry (you get lots of bad CRC packets)
+
             const uint8_t settings_auto_bind = 0;
             const uint8_t settings_rx_num = 0;
             const uint8_t settings_low_power = 0;
-            const uint8_t settings_option = 0;
+
+#if 0   
+Bayang send packet code from multiprotocol module source code
+CH5_SW thru CH13_SW is used. These are defined as:
+#define CH5_SW	(Channel_AUX & _BV(0))      -> BAYANG_FLAG_FLIP
+#define CH6_SW	(Channel_AUX & _BV(1))      -> BAYANG_FLAG_RTH
+#define CH7_SW	(Channel_AUX & _BV(2))      -> BAYANG_FLAG_PICTURE
+#define CH8_SW	(Channel_AUX & _BV(3))      -> BAYANG_FLAG_VIDEO
+#define CH9_SW	(Channel_AUX & _BV(4))      -> BAYANG_FLAG_HEADLESS
+#define CH10_SW	(Channel_AUX & _BV(5))      -> BAYANG_FLAG_INVERTED
+#define CH11_SW	(Channel_AUX & _BV(6))      -> dyntrim = 0
+#define CH12_SW	(Channel_AUX & _BV(7))      -> BAYANG_FLAG_TAKE_OFF
+#define CH13_SW	(Channel_data[CH13]>CHANNEL_SWITCH) -> BAYANG_FLAG_EMG_STOP
+
+
+Channel_AUX is initialized from channel data as follows:
+
+	//Calc AUX flags
+	Channel_AUX=0;
+	for(uint8_t i=0;i<8;i++)
+		if(Channel_data[CH5+i]>CHANNEL_SWITCH)
+			Channel_AUX|=1<<i;
+
+> Note: CHANNEL_SWITCH is 1104
+
+// Also, I'm using these settings when I build SilverWare NFE
+//
+//#define ARMING CHAN_5     BAYANG_FLAG_INVERT (packet[3]), SwA
+//#define IDLE_UP CHAN_9    BAYANG_FLAG_HEADLESS (packet[2]), SwD
+//#define LEVELMODE CHAN_6  BAYANG_FLAG_FLIP (packet[2]), SwB: Position 1
+//#define RACEMODE  CHAN_7  BAYANG_FLAG_SNAPSHOT (packet[2]), SwC: Position 2
+//#define HORIZON   CHAN_8  BAYANG_FLAG_VIDEO (packet[2]), SwC: Position 3
+
+// ADC_ID_SwB == SwB: Position 1 == <100(46), Position 2 == >923(1023)
+// ADC_ID_SwC == SwC: Position 1 == 0, Position 2 == 512, Position 3 == 1023
+
+
+		//Flags packet[2]
+		packet[2] = 0x00;
+		if(CH5_SW)
+			packet[2] = BAYANG_FLAG_FLIP;
+		if(CH6_SW)
+			packet[2] |= BAYANG_FLAG_RTH;
+		if(CH7_SW)
+			packet[2] |= BAYANG_FLAG_PICTURE;
+		if(CH8_SW)
+			packet[2] |= BAYANG_FLAG_VIDEO;
+		if(CH9_SW)
+		{
+			packet[2] |= BAYANG_FLAG_HEADLESS;
+			dyntrim = 0;
+		}
+		//Flags packet[3]
+		packet[3] = 0x00;
+		if(CH10_SW)
+			packet[3] = BAYANG_FLAG_INVERTED;
+		if(CH11_SW)
+			dyntrim = 0;
+		if(CH12_SW)
+		  packet[3] |= BAYANG_FLAG_TAKE_OFF;
+		if(CH13_SW)
+			packet[3] |= BAYANG_FLAG_EMG_STOP;
+
+
+So something like this will be needed
+
+    //#define ARMING CHAN_5     BAYANG_FLAG_INVERT (packet[3]), SwA
+    const int CH10_SW = 9;
+    adc_data[CH10_SW]= button_active(kBtn_SwA) ? 4095 : 0;
+
+    //#define IDLE_UP CHAN_9    BAYANG_FLAG_HEADLESS (packet[2]), SwD
+    const int CH9_SW = 8;
+    adc_data[CH9_SW]= button_active(kBtn_SwD) ? 4095 : 0;
+
+    //#define LEVELMODE CHAN_6  BAYANG_FLAG_FLIP (packet[2]), SwB: Position 1
+    const int CH5_SW = 4;
+    adc_data[CH5_SW]= (adc_get_channel_calibrated(ADC_ID_SwB) < 100) ? 4095 : 0;
+
+    //#define RACEMODE  CHAN_7  BAYANG_FLAG_SNAPSHOT (packet[2]), SwC: Position 2
+    const uint16_t SwC = adc_get_channel_calibrated(ADC_ID_SwC);
+    const int CH7_SW = 6;
+    adc_data[CH7_SW]= (SwC >= 412) ? 4095 : 0;
+
+    //#define HORIZON   CHAN_8  BAYANG_FLAG_VIDEO (packet[2]), SwC: Position 3
+    const int CH8_SW = 7;
+    adc_data[CH8_SW]= (SwC >= 923) ? 4095 : 0;
+
+    // Disable dyntrim (because I don't know what it does exactly and it seems wrong to have it on based on code in Silverware)
+    const int CH11_SW = 10;
+    adc_data[CH11_SW] = 4095;
+
+#endif            
 
             // Header
             *p++ = settings_protocol < 32 ? 0x55 : 0x54;
@@ -267,7 +361,7 @@ void multiprotocol_update(void)
             uint32_t bits = 0;
             uint8_t bitsavailable = 0;
 
-            // fetch adc channel data
+            // fetch adc channel data for the sticks
             uint16_t adc_data[8];
             for (int i = ADC_ID_AILERON; i <= ADC_ID_SwC; i++) 
             {
@@ -276,14 +370,67 @@ void multiprotocol_update(void)
                 adc_data[i] = value;
             }
 
+            // Multiprotocol module expects 16 channels to be provided
+            const uint16_t SwC = adc_get_channel_calibrated(ADC_ID_SwC);
             for (int i = 0; i < MULTI_CHANS; i++) 
             {
-            	// adc values:
+                // Stick adc values:
             	// 0	-100%
             	// 2048    0%
             	// 4095	+100%
                 int value = (i <= ADC_ID_SwC) ? adc_data[i] : 0;
-                value -= 2047;
+
+                if (settings_protocol == kBayangProtocol)
+                {
+                    // The following channels correspond to various
+                    // Bayang flags that we care about
+                    enum
+                    {
+                        CH5_SW  = 4,
+                        CH7_SW  = 6,
+                        CH8_SW  = 7,
+                        CH9_SW  = 8,
+                        CH10_SW = 9,
+                        CH11_SW = 10
+                    };
+
+                    // On my NFE Silverware quads I setup ARMING, IDLE_UP, etc
+                    // to virtual channels (CHAN_5, etc) which in turn are initialized
+                    // from Bayang flags. The comments below (above each case
+                    // statement) describe how I expect things to be mapped.
+                    switch (i)
+                    {
+                        //#define ARMING CHAN_5     BAYANG_FLAG_INVERT, SwA
+                        case CH10_SW:
+                            value = button_active(kBtn_SwA) ? 4095 : 0;
+                            break;
+
+                        //#define IDLE_UP CHAN_9    BAYANG_FLAG_HEADLESS, SwD
+                        case CH9_SW:
+                            value = button_active(kBtn_SwD) ? 4095 : 0;
+                            break;
+
+                        //#define LEVELMODE CHAN_6  BAYANG_FLAG_FLIP, SwB: Position 1
+                        case CH5_SW:
+                            value = (adc_get_channel_calibrated(ADC_ID_SwB) < 100) ? 4095 : 0;
+                            break;
+
+                        //#define RACEMODE  CHAN_7  BAYANG_FLAG_SNAPSHOT, SwC: Position 2
+                        case CH7_SW:
+                            value = (SwC >= 412) ? 4095 : 0;
+                            break;
+
+                        //#define HORIZON   CHAN_8  BAYANG_FLAG_VIDEO, SwC: Position 3
+                        case CH8_SW:
+                            value = (SwC >= 923) ? 4095 : 0;
+                            break;
+
+                        // Disable dyntrim (because I don't know what it does exactly and it seems wrong to have it on based on code in Silverware)
+                        case CH11_SW:
+                            value = 4095;
+                            break;
+                    }
+                }
 
                 // Channel value constrained to 11 bits (0 to 2047 inclusive)
                 //	0		-125%
@@ -291,6 +438,9 @@ void multiprotocol_update(void)
                 //	1024	   0%
                 //	1843	+100%
                 //	2047	+125%
+
+                // (4095 - 2048) * 820 / 2048 + 1024 => 1843
+                value -= 2048;
                 // Scale to 82% (1024-204 == 820)
                 value = value * 820 / 2048 + 1024;
                 bits |= constrain(value, 0, 2047) << bitsavailable;
